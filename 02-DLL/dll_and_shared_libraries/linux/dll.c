70fe9a3e758490ea1d963defda9c2f50694d2df5
Vinicius Trindade <contato@viniciustrindade.com.br>
2016-03-04 21:26:10 -0300

    Segundo Commit

---
+++ 02-DLL/dll_and_shared_libraries/linux/dll.c
@@ -0,0 +1,15 @@
+#define BUILDDLL
+#include <stdio.h>
+#include "dll.h"
+
+MINHADLL int soma(int a,int b)
+{
+    int res = a+b;
+    return res;
+}
+
+MINHADLL int sub(int a,int b)
+{
+     int res = a-b;
+     return res;
+}
